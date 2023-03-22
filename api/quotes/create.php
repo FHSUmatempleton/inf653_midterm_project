3c3eb6a13221dc6b15d4eb8c0425aff829979eec
FHSUmatempleton <matempleton@mail.fhsu.edu>
2023-03-22 16:51:15 -0400

    update quote create

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -24,9 +24,16 @@
 
   // Create quote
   if($quote->create()) {
-    echo json_encode(
-      array('message' => 'Quote Created')
+    // Create array
+    $quote_arr = array(
+      'id' => $quote->id,
+      'quote' => $quote->quote,
+      'author_id' => $quote->author_id,
+      'category_id' => $quote->category_id
     );
+    
+    // Make JSON
+    print_r(json_encode($quote_arr));
   } else {
     echo json_encode(
       array('message' => 'Quote Not Created')
