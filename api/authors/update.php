edfeba3af3122d35952d1dcbeb622311439297e7
FHSUmatempleton <matempleton@mail.fhsu.edu>
2023-03-22 02:48:27 -0400

    updated author update

--- api/authors/update.php
+++ api/authors/update.php
@@ -16,7 +16,13 @@
 
   // Get raw posted data
   $data = json_decode(file_get_contents("php://input"));
-
+  if (!isset($data->id) || !isset($data->author)) {
+    echo json_encode(
+      array('message' => 'Missing Required Parameters')
+    );
+    die();
+  }
+  
   // Set ID to UPDATE
   $author->id = $data->id;
 
--- api/categories/create.php
+++ api/categories/create.php
@@ -16,13 +16,13 @@
 
   // Get raw posted data
   $data = json_decode(file_get_contents("php://input"));
-  if (!isset($data->author)) {
+  if (!isset($data->category)) {
     echo json_encode(
       array('message' => 'Missing Required Parameters')
     );
     die();
   }
-  
+
   $category->category = $data->category;
 
   // Create Category
