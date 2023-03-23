62337f5b974eddb6ce15defb55c05269ef638b02
FHSUmatempleton <matempleton@mail.fhsu.edu>
2023-03-22 20:30:01 -0400

    update quote create

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -17,7 +17,13 @@
 
   // Get raw quoteed data
   $data = json_decode(file_get_contents("php://input"));
-
+  if (!isset($data->quote) || !isset($data->author_id) || !isset($data->category_id)) {
+    echo json_encode(
+      array('message' => 'Missing Required Parameters')
+    );
+    die();
+  }
+  
   $quote->quote = $data->quote;
   $quote->author_id = $data->author_id;
   $quote->category_id = $data->category_id;
