ce0ce706e49873df1236ce7d30e5f5a96bd461bd
FHSUmatempleton <matempleton@mail.fhsu.edu>
2023-03-22 21:56:17 -0400

    update category read

--- api/categories/read.php
+++ api/categories/read.php
@@ -23,7 +23,6 @@
   if($num > 0) {
         // Cat array
         $cat_arr = array();
-        // $cat_arr['data'] = array();
 
         while($row = $result->fetch(PDO::FETCH_ASSOC)) {
           extract($row);
@@ -33,9 +32,7 @@
             'category' => $category
           );
 
-          // Push to "data"
           array_push($cat_arr, $cat_item);
-          // array_push($cat_arr['data'], $cat_item);
         }
 
         // Turn to JSON & output
