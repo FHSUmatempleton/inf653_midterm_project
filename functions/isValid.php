61d1e194fca8a2ba391eef5d2f8fd75722d4d0a7
FHSUmatempleton <matempleton@mail.fhsu.edu>
2023-03-22 22:00:47 -0400

    update function isValid

--- functions/isValid.php
+++ functions/isValid.php
@@ -1,4 +1,5 @@
 <?php
+    // This function verifies that an entry with param $id exists within the param $model
     function isValid($id, $model) {
         $model->id = $id;
         $result = $model->read_single();
